e45100d8d431de42ceeb7b70c40a83695b33a65f
Ex333 <haldas.mateusz@gmail.com>
2026-01-12 20:10:50 +0100

    Next commit im testing templates

--- filmy/urls.py
+++ filmy/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import filmy, ulubione
+from .views import filmy, ulubione, test
 
 urlpatterns = [
     path("", filmy, name="filmy"),
     path('ulubione/', ulubione, name='ulubione'),
+    path("test/",test, name='test')
 ]
--- filmy/views.py
+++ filmy/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def filmy(request):
     return render(request, "filmy.html")
 
 def ulubione(request):
     return render(request, "ulubione.html")
+
+def test(request):
+    return render(request, "test.html")
