f267840fda6217c8b4aedf10b784d9b8e40649fe
Ex333 <haldas.mateusz@gmail.com>
2026-01-13 18:57:51 +0100

    models, admin has changed

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Film
-# Register your models here.
-admin.site.register(Film)
+
+#admin.site.register(Film) You can register the model like this or better like below:
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ("id", "tytul", "rok_produkcji", "kategoria")
+    search_fields = ("tytul", "kategoria", "rok_produkcji")
+    list_filter = ("kategoria", "rok_produkcji")
--- filmy/models.py
+++ filmy/models.py
@@ -1,4 +1,25 @@
 from django.db import models
 
 class Film(models.Model):
-    tytuł =models.CharField(max_length=64)
+
+    class Kategoria(models.TextChoices):
+        AKCJA = "akcja", "Akcja"
+        KOMEDIA = "komedia", "Komedia"
+        DRAMAT = "dramat", "Dramat"
+        HORROR = "horror", "Horror"
+        SCI_FI = "sci-fi", "Sci-Fi"
+        FANTASY = "fantasy", "Fantasy"
+        ANIMACJA = "animacja", "Animacja"
+        DOKUMENTALNY = "dokumentalny", "Dokumentalny"
+
+    tytul = models.CharField(max_length=64, unique=True)
+    opis = models.TextField(blank=True)
+    rok_produkcji = models.PositiveSmallIntegerField(null=True, blank=True)
+    kategoria = models.CharField(max_length=32, choices=Kategoria.choices)
+    zdjecie = models.ImageField(upload_to="filmy/", null=True, blank=True)
+
+    def __str__(self):
+        return self.tytul + str(self.rok_produkcji)
+    
+    class Meta:
+        ordering = ['tytul']
