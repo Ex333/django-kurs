34d42010da32915e42a17638b98ee2142584624f
Ex333 <haldas.mateusz@gmail.com>
2026-01-13 14:52:23 +0100

    Added model Filmy

--- filmy/models.py
+++ filmy/models.py
@@ -1,14 +1,4 @@
 from django.db import models
 
-# Create your models here.
 class Film(models.Model):
-    tytul = models.CharField(max_length=100,blank=False, unique=True)
-    rok = models.IntegerField(blank=False)
-    opis = models.TextField(blank=True)
-    premiera = models.DateField(blank=True, null=True)
-    imdb_rating = models.DecimalField(max_digits=3, decimal_places=1, blank=True, null=True)
-    plakat = models.ImageField(upload_to='plakaty/', blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.tytul} ({self.rok})"
-    
+    tytuł =models.CharField(max_length=64)
