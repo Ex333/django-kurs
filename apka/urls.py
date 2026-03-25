474956b7ba346af60eb4deddc6aee495935ad23d
Ex333 <haldas.mateusz@gmail.com>
2026-03-25 16:36:20 +0100

    Commit

--- apka/urls.py
+++ apka/urls.py
@@ -3,6 +3,5 @@ from .views import filmy, ulubione, test
 
 urlpatterns = [
     path("", filmy, name="filmy"),
-    path('ulubione/', ulubione, name='ulubione'),
-    path("test/",test, name='test')
 ]
+    
